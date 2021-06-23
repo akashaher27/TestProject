ed17d9af333bec26dfabeae6fea0a8b3eef09ae2
akash <akash.aher@mediassist.in>
2021-06-23 13:06:41 +0530

    Added UserStore

---
+++ app/src/main/java/com/example/testproject/data/BaseSharePref.kt
@@ -0,0 +1,43 @@
+package com.example.testproject.data
+
+import android.content.SharedPreferences
+import androidx.core.content.edit
+
+/**
+ * Created by akash on 22,06,2021
+ */
+abstract class BaseSharePref {
+
+    /**
+     * puts a key value pair in shared prefs if doesn't exists, otherwise updates value on given [key]
+     */
+    fun SharedPreferences.setValue(key: String, value: Any?) {
+        when (value) {
+            is String? -> edit { putString(key, value) }
+            is Int -> edit { putInt(key, value) }
+            is Boolean -> edit { putBoolean(key, value) }
+            is Float -> edit { putFloat(key, value) }
+            is Long -> edit { putLong(key, value) }
+            else -> throw UnsupportedOperationException("Not yet implemented")
+        }
+    }
+
+    /**
+     * finds value on given key.
+     * [T] is the type of value
+     * @param defaultValue optional default value - will take null for strings, false for bool and -1 for numeric values if [defaultValue] is not specified
+     */
+    inline  fun <reified T : Any> SharedPreferences.getValue(key: String, defaultValue: T? = null): T? {
+        return when (T::class) {
+            String::class -> getString(key, defaultValue as? String) as T?
+            Int::class -> getInt(key, defaultValue as? Int ?: -1) as T?
+            Boolean::class -> getBoolean(key, defaultValue as? Boolean ?: false) as T?
+            Float::class -> getFloat(key, defaultValue as? Float ?: -1f) as T?
+            Long::class -> getLong(key, defaultValue as? Long ?: -1) as T?
+            else -> throw UnsupportedOperationException("Not yet implemented")
+        }
+    }
+
+
+    abstract fun clearStore()
+}
---
+++ app/src/main/java/com/example/testproject/data/UserStore.kt
@@ -0,0 +1,46 @@
+package com.example.testproject.data
+
+import android.content.Context
+import android.content.SharedPreferences
+
+/**
+ * Created by akash on 20,06,2021
+ */
+class UserStore(context: Context) : BaseSharePref() {
+
+    companion object {
+        const val PREF_NAME = "USER_STORE"
+
+        private const val KEY_USER_NAME = "KEY_USER_NAME"
+        private const val KEY_USER_AGE = "KEY_USER_AGE"
+        private const val KEY_USER_PINCODE = "KEY_USER_PINCODE"
+        private const val KEY_USER_IS_USER_REGISTER = "KEY_USER_IS_USER_REGISTER"
+    }
+
+    private val preferences: SharedPreferences by lazy {
+        context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+    }
+
+
+    var username: String?
+        get() = preferences.getValue(KEY_USER_NAME)
+        set(value) = preferences.setValue(KEY_USER_NAME, value)
+
+    var userAge: Int?
+        get() = preferences.getValue(KEY_USER_AGE)
+        set(value) = preferences.setValue(KEY_USER_AGE, value)
+
+    var userPincode: Float?
+        get() = preferences.getValue(KEY_USER_PINCODE)
+        set(value) = preferences.setValue(KEY_USER_PINCODE, value)
+
+    var isUserRegister: Boolean?
+        get() = preferences.getValue(KEY_USER_IS_USER_REGISTER)
+        set(value) = preferences.setValue(KEY_USER_IS_USER_REGISTER, value)
+
+
+    override fun clearStore() {
+        preferences.edit().clear().apply()
+    }
+
+}
