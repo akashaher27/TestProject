4d7473b324dc079517fc214ed4961965798237d3
akash <akash.aher@mediassist.in>
2021-06-29 11:02:05 +0530

    Added AppStore and UserStore

--- app/src/main/java/com/example/testproject/data/UserStore.kt
+++ app/src/main/java/com/example/testproject/data/UserStore.kt
@@ -2,6 +2,8 @@ package com.example.testproject.data
 
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -14,18 +16,20 @@ import javax.inject.Singleton
 class UserStore @Inject constructor(@ApplicationContext context: Context) : BaseSharePref() {
 
     companion object {
-        const val PREF_NAME = "USER_STORE"
-
+        private const val STORE_NAME = "USER_STORE"
         private const val KEY_USER_NAME = "KEY_USER_NAME"
         private const val KEY_USER_AGE = "KEY_USER_AGE"
         private const val KEY_USER_PINCODE = "KEY_USER_PINCODE"
         private const val KEY_USER_IS_USER_REGISTER = "KEY_USER_IS_USER_REGISTER"
     }
 
-    private val preferences: SharedPreferences by lazy {
-        context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
-    }
-
+    private val preferences: SharedPreferences = EncryptedSharedPreferences.create(
+        STORE_NAME,
+        MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC),
+        context,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+    )
 
     var username: String?
         get() = preferences.getValue(KEY_USER_NAME)
